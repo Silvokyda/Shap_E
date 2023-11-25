94932c0470833725f69405a4b34497da84d98f5e
Silvanus Oketch <deseve48@gmail.com>
2023-11-25 20:27:20 +0300

    api-flask

--- main.py
+++ main.py
@@ -1,13 +1,44 @@
-from flask import Flask, jsonify
-import os
+from flask import Flask, request, jsonify
+from diffusers import DiffusionPipeline, ShapEImg2ImgPipeline, export_to_gif
+from PIL import Image
 
 app = Flask(__name__)
 
+# Load models
+prior_pipeline = DiffusionPipeline.from_pretrained("kandinsky-community/kandinsky-2-1-prior", torch_dtype=torch.float16, use_safetensors=True).to("cuda")
+pipeline = DiffusionPipeline.from_pretrained("kandinsky-community/kandinsky-2-1", torch_dtype=torch.float16, use_safetensors=True).to("cuda")
+shape_img2img_pipeline = ShapEImg2ImgPipeline.from_pretrained("openai/shap-e-img2img", torch_dtype=torch.float16, variant="fp16").to("cuda")
 
-@app.route('/')
-def index():
-    return jsonify({"Choo Choo": "Welcome to your Flask app 🚅"})
+@app.route('/generate_image', methods=['POST'])
+def generate_image():
+    data = request.json
+
+    # Check if the request contains a prompt or an image file
+    if 'prompt' in data:
+        prompt = data['prompt']
+
+        # Generate image using the provided prompt
+        image_embeds, negative_image_embeds = prior_pipeline(prompt, guidance_scale=1.0).to_tuple()
+        generated_image = pipeline(prompt, image_embeds=image_embeds, negative_image_embeds=negative_image_embeds).images[0]
+
+    elif 'image' in request.files:
+        # If an image file is provided, process it
+        uploaded_image = Image.open(request.files['image']).resize((256, 256))
+        guidance_scale = 3.0
+
+        # Generate 3D image (GIF) from the 2D image
+        images = shape_img2img_pipeline(uploaded_image, guidance_scale=guidance_scale, num_inference_steps=64, frame_size=256).images
+        generated_image = export_to_gif(images[0], None)
+
+    else:
+        return jsonify({'error': 'Invalid request'}), 400
+
+    # Save and return the generated image
+    generated_image_path = "generated_image.png"
+    generated_image.save(generated_image_path)
+
+    return jsonify({'generated_image_path': generated_image_path})
 
 
 if __name__ == '__main__':
-    app.run(debug=True, port=os.getenv("PORT", default=5000))
+    app.run(debug=True)
